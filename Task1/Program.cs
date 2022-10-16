9adee82f17830680e69e4f5aefa81f519235c734
afromarine@afromarine-VirtualBox <dan@gmail.com>
2022-10-16 17:23:34 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter the five-digit number");
+String number=Console.ReadLine();
+
+void CheckingNumber (String number){
+    if (number[0]==number[4]&&number[1]==number[3])
+    Console.WriteLine("Yes");
+    else Console.WriteLine("No");
+    };
+
+if (number.Length>5||number.Length<5)
+    Console.WriteLine("This is not five-digit number");
+else CheckingNumber(number);
